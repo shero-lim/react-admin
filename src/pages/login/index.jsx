0cc7b4907a8108f031ee9c4bad43cbb3ac18d516
shero.lin <2390378545@qq.com>
2023-05-22 01:00:06 +0800

    init

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import "./index.less"
+
+export default class Login extends Component{
+    render(){
+        return (
+            <div className="login">
+                <header className="login-header"></header>
+                <section className="login-content"></section>
+            </div>
+        )
+    }
+}
